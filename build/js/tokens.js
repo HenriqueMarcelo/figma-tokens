dfa5c98ed25ab833d2b2df073a6550222c8dc0d4
Marcelo Henrique Fonseca <marcelohenriquedasilvafonseca@gmail.com>
2023-02-01 10:22:35 -0300

    Initial commit

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Wed, 01 Feb 2023 12:57:17 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray200": "#fafafa",
+  "ColorGray500": "#71717a",
+  "ColorGray800": "#27272a",
+  "ColorGray900": "#18181b",
+  "ColorPurple500": "#8b5cf6"
+};
